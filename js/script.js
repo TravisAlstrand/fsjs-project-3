19c39cae5d72699c714436fb818bf7a43d07aac5
TravisAlstrand <travisalstrand@gmail.com>
2022-03-09 10:23:08 -0800

    Commit 7
    Payment and Submission finished.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,27 @@
+// name
 const nameField = document.getElementById('name');
-
+// job title
 const jobSelect = document.getElementById('title');
 const otherJobText = document.getElementById('other-job-role');
-
+// t-shirt
 const shirtDesignSelect = document.getElementById('design');
 const shirtColorSelect = document.getElementById('color');
 const shirtColors = document.querySelectorAll('option[data-theme]');
-
+// activities
 const activitiesField = document.getElementById('activities');
 const totalCostDisplay = document.getElementById('activities-cost');
 let totalCost = 0;
+// payment
+const paymentSelect = document.getElementById('payment');
+const creditCardDiv = document.getElementById('credit-card');
+const paypalDiv = document.getElementById('paypal');
+const bitcoinDiv = document.getElementById('bitcoin');
+// submission
+const emailField = document.getElementById('email');
+const cardNumField = document.getElementById('cc-num');
+const zipField = document.getElementById('zip');
+const cvvField = document.getElementById('cvv');
+const formElement = document.querySelector('form');
 
 // *****************************************************************
 // --NAME FIELD--
@@ -115,3 +127,95 @@ activitiesField.addEventListener('change', (e) => {
 function updateCost(total) {
     totalCostDisplay.innerHTML = `Total: $${total}`;
 }
+
+// *****************************************************************
+// --Payment--
+// *****************************************************************
+
+// create array of payment options
+const paymentOptions = paymentSelect.children;
+
+// create array of payment divs
+const paymentDivs = [
+    'select payment placeholder',
+    creditCardDiv,
+    paypalDiv,
+    bitcoinDiv
+];
+
+// set credit-card as default payment upon loading page
+paymentOptions[1].setAttribute('selected', 'selected');
+
+// hide the bitcoin and paypal divs initially
+paypalDiv.style.display = 'none';
+bitcoinDiv.style.display = 'none';
+
+// listen for payment selection
+paymentSelect.addEventListener('change', () => {
+
+    // iterate through payment options array
+    // set selected option as 'selected' remove attribute from others
+    // show payment div of equal index value & hide others
+    for (i = 1; i < paymentOptions.length; i++) {
+        if (paymentSelect.value === paymentOptions[i].value) {
+            paymentOptions[i].setAttribute('selected', 'selected');
+            paymentDivs[i].style.display = 'inherit';
+        } else {
+            paymentOptions[i].removeAttribute('selected');
+            paymentDivs[i].style.display = 'none';
+        }
+    }
+});
+
+// *****************************************************************
+// --Submission--
+// *****************************************************************
+
+// check name validity, return bool
+function checkName(name) {
+    return /^[a-z]+\s?[a-z]+$/i.test(name);
+}
+
+function checkEmail(email) {
+    return /^[^@]+@[^@.]+\.[a-z]+$/i.test(email);
+}
+
+function checkZip(zip) {
+    return /^(\d{5})$/.test(zip);
+}
+
+function checkCVV(cvv) {
+    return /^(\d{3})$/.test(cvv);
+}
+
+// listen for the submit event
+formElement.addEventListener('submit', (e) => {
+
+    // create variables for field values
+    const userName = nameField.value;
+    const userEmail = emailField.value;
+    const userZip = zipField.value;
+    const userCVV = cvvField.value;
+
+    // call validator functions passing variables above
+    const isValidName = checkName(userName);
+    const isValidEmail = checkEmail(userEmail);
+    const isValidZip = checkZip(userZip);
+    const isValidCVV = checkCVV(userCVV);
+    
+    // check if fields are valid, prevent submission if false
+    if (isValidName === false || 
+        isValidEmail === false ||
+        isValidZip === false ||
+        isValidCVV === false ||
+        totalCost === 0) 
+    {
+        e.preventDefault();
+        console.log(false);
+    }
+});
+
+// *****************************************************************
+// --Accessibility--
+// *****************************************************************
+
